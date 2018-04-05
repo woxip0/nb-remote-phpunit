6e93a644659f3ff615a241eabad1a811027412dc
Dixon Chacón <gocho.14@gmail.com>
2018-04-05 16:49:02 -0400

    Removed old paths.

--- netbeans/phpunit-docker.php
+++ netbeans/phpunit-docker.php
@@ -5,10 +5,15 @@ namespace Foogile\NetBeans\PhpUnit;
 set_include_path(get_include_path() . ':' . __DIR__ . '/../');
 require 'vendor/autoload.php';
 
-$pathMappings = array ('/home/dixon/NetBeansProjects/PhpProject1/' => '/var/www/html/');
+// EDITME:
+$localWorkingDirectory = '';
+$dockerName = '';
+
+
+$pathMappings = array ($localWorkingDirectory => '/var/www/html/');
 $tmpPath = '/tmp';
 $remotePhpUnitPath = '/usr/bin/phpunit';
-$dockerName = "devtest";
+
 
 $testRunner = new TestRunner(
     new ArgumentParser(),
